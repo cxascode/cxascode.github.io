7123cd9658c18d1907c75cd97ea6577eb65b2957
Adam Scheblein <adamscheblein@gmail.com>
2025-12-19 19:45:53 -0500

    wrapping in genesys look-n-feel

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./App.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { registerElements } from "genesys-spark-components";
+registerElements(); // required so <gux-*> components work in the browser  [oai_citation:1‡GitHub](https://github.com/MyPureCloud/genesys-spark/blob/main/packages/genesys-spark-components-react/README.md?utm_source=chatgpt.com)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
